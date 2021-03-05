e8c3b4ba6bca654db9b25ef2e7eb31e44a530526
ayxliu19 <40506840+ayxliu19@users.noreply.github.com>
2021-03-05 15:29:25 -0500

    Stepper Motor updated

---
+++ Stepper_Motor/Stepper_Motor.ino
@@ -0,0 +1,53 @@
+
+/*
+ Stepper Motor Control - one revolution
+
+ This program drives a unipolar or bipolar stepper motor.
+ The motor is attached to digital pins 8 - 11 of the Arduino.
+
+ The motor should revolve one revolution in one direction, then
+ one revolution in the other direction.
+
+
+ Created 11 Mar. 2007
+ Modified 30 Nov. 2009
+ by Tom Igoe
+
+ */
+
+#include <Stepper.h>
+
+// servo rotations based on notes
+#define Cserv 0
+#define Dserv 37
+#define Eserv 73
+#define Fserv 110
+#define Gserv 146
+#define Aserv 183
+#define Bserv 219
+#define high_Cserv 255
+
+int servoNotes[] = {Cserv, Cserv, Cserv, Cserv, Cserv, Cserv, Dserv, Dserv, Eserv, Eserv, Eserv, Eserv, Dserv, Dserv, Eserv, Eserv, Fserv, Fserv, Gserv, Gserv, high_Cserv, high_Cserv, high_Cserv, high_Cserv, high_Cserv, high_Cserv, Gserv, Gserv, Gserv, Gserv, Gserv, Gserv, Eserv, Eserv, Eserv, Eserv, Eserv, Eserv, Cserv, Cserv, Cserv, Cserv, Cserv, Cserv, Gserv, Gserv, Fserv, Fserv, Eserv, Eserv, Dserv, Dserv, Cserv, Cserv};
+double degree = 7.5;
+int currAngle = 48;
+
+Stepper myStepper(currAngle, 4, 5, 6, 7);
+
+void setup() {
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(60);
+  // initialize the serial port:
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (int angle : servoNotes) {
+    double difference = (angle - currAngle)/degree;
+    Serial.println(angle);
+    Serial.println(currAngle);
+    Serial.println("clockwise");
+    myStepper.step(difference);
+    delay(100);
+    currAngle = angle;
+  }
+}
