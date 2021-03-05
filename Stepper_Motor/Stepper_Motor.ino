07aca2021576c035e5b47f909cd0bace678dd062
unknown <wbkennedy1@gmail.com>
2021-03-05 16:48:25 -0500

    changed stepper motor code to work with 1.8 degree motor

--- Stepper_Motor/Stepper_Motor.ino
+++ Stepper_Motor/Stepper_Motor.ino
@@ -28,14 +28,14 @@
 #define high_Cserv 255
 
 int servoNotes[] = {Cserv, Cserv, Cserv, Cserv, Cserv, Cserv, Dserv, Dserv, Eserv, Eserv, Eserv, Eserv, Dserv, Dserv, Eserv, Eserv, Fserv, Fserv, Gserv, Gserv, high_Cserv, high_Cserv, high_Cserv, high_Cserv, high_Cserv, high_Cserv, Gserv, Gserv, Gserv, Gserv, Gserv, Gserv, Eserv, Eserv, Eserv, Eserv, Eserv, Eserv, Cserv, Cserv, Cserv, Cserv, Cserv, Cserv, Gserv, Gserv, Fserv, Fserv, Eserv, Eserv, Dserv, Dserv, Cserv, Cserv};
-double degree = 7.5;
-int currAngle = 48;
+double degree = 1.8;
+int currAngle = 200;
 
 Stepper myStepper(currAngle, 4, 5, 6, 7);
 
 void setup() {
   // set the speed at 60 rpm:
-  myStepper.setSpeed(60);
+  myStepper.setSpeed(200);
   // initialize the serial port:
   Serial.begin(9600);
 }
